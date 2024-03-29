25f0901a568de4bad3e265f55a453aa053c69773
aisyarahmar <aisyarahmar06@gmail.com>
2024-03-29 21:38:23 +0700

    PBI#1-b_CRUD_Menu

---
+++ resources/views/create.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Menu</title>
+</head>
+<body>
+    <header>
+        <h1>Daftar Menu</h1>
+        <p>Nama Toko</p> <!--gimana yak cara masukin nama tokonya??-->
+    </header>
+
+    <div>
+        <h3>Menambah Menu</h3>
+        <form action="/" method="POST"> 
+            <!-- @csrf -->
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nama Produk</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Deskripsi Produk</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Harga Satuan</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Stok</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="imageInput">Choose Image</label>
+                <input type="file" class="form-control-file" id="imageInput" name="image">
+            </div>
+            <input type="submit" name="submit" class="btn btn-primary" value="Save"> <br> 
+        </form>
+    </div>
+</body>
+</html>

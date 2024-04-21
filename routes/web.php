233fb7ddce9de5fb4dac54f10645123b0d401833
Froyotive <evandanirafif@gmail.com>
2024-04-21 23:02:12 +0700

    SI45KEL06-39 Update Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\MitraController;
-
+use App\Http\Controllers\PartnerRegistrationController;
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,4 +23,29 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'index'])->name('profile.index');
+Route::get('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+Route::get('/edit_profile', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+Route::post('/edit_profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.edit');
+
+Route::controller(PartnerRegistrationController::class)->name('registration.')->group(function () {
+    Route::get('/registration', 'index')->name('index');
+    Route::post('/store', 'store')->name('store');
+});
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::controller(AdminDashboardController::class)->middleware(['auth', 'admin'])->name('dashboard.')->group(function () {
+        Route::get('/dashboard', 'index')->name('index');
+        Route::get('/view/{uuid}', 'view')->name('view');
+        Route::post('/view/update-status', 'updateStatus')->name('updateStatus');
+        Route::post('/view/delete', 'delete')->name('delete');
+    });
+
+    Route::controller(AdminAuthController::class)->name('auth.')->group(function () {
+        Route::get('/login', 'index')->middleware('guest')->name('login');
+        Route::post('/login/store', 'store')->middleware('guest')->name('store');
+        Route::post('/logout', 'logout')->middleware('auth')->name('logout');
+    });
+});
+
 Route::view('mitra', 'mitra.dashboard');
